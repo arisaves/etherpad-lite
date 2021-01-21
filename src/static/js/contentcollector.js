6217682b0370626ad88e9e354c1b6f19591630fb
Richard Hansen <rhansen@rhansen.org>
2021-01-21 02:35:44 -0500

    contentcollector: Invert logic to improve readability

--- src/static/js/contentcollector.js
+++ src/static/js/contentcollector.js
@@ -212,24 +212,23 @@ const makeContentCollector = (collectStyles, abrowser, apool, className2Author) 
   const _recalcAttribString = (state) => {
     const lst = [];
     for (const [a, count] of Object.entries(state.attribs)) {
-      if (count) {
-        // The following splitting of the attribute name is a workaround
-        // to enable the content collector to store key-value attributes
-        // see https://github.com/ether/etherpad-lite/issues/2567 for more information
-        // in long term the contentcollector should be refactored to get rid of this workaround
-        const ATTRIBUTE_SPLIT_STRING = '::';
+      if (!count) continue;
+      // The following splitting of the attribute name is a workaround
+      // to enable the content collector to store key-value attributes
+      // see https://github.com/ether/etherpad-lite/issues/2567 for more information
+      // in long term the contentcollector should be refactored to get rid of this workaround
+      const ATTRIBUTE_SPLIT_STRING = '::';
 
-        // see if attributeString is splittable
-        const attributeSplits = a.split(ATTRIBUTE_SPLIT_STRING);
-        if (attributeSplits.length > 1) {
-          // the attribute name follows the convention key::value
-          // so save it as a key value attribute
-          lst.push([attributeSplits[0], attributeSplits[1]]);
-        } else {
-          // the "normal" case, the attribute is just a switch
-          // so set it true
-          lst.push([a, 'true']);
-        }
+      // see if attributeString is splittable
+      const attributeSplits = a.split(ATTRIBUTE_SPLIT_STRING);
+      if (attributeSplits.length > 1) {
+        // the attribute name follows the convention key::value
+        // so save it as a key value attribute
+        lst.push([attributeSplits[0], attributeSplits[1]]);
+      } else {
+        // the "normal" case, the attribute is just a switch
+        // so set it true
+        lst.push([a, 'true']);
       }
     }
     if (state.authorLevel > 0) {
@@ -462,12 +461,9 @@ const makeContentCollector = (collectStyles, abrowser, apool, className2Author) 
               // HTMLCollection implements the iterable protocol, so for..of iteration should always
               // work. See: https://stackoverflow.com/a/41759532. Cheerio should behave the same.
               for (const child of node.children) {
-                if (child.name === 'ul') {
-                  type = child.attribs.class;
-                  if (type) {
-                    break;
-                  }
-                }
+                if (child.name !== 'ul') continue;
+                type = child.attribs.class;
+                if (type) break;
               }
             }
             if (rr && rr[1]) {
